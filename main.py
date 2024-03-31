07af1add7196482376490ce084e56b7892a0fb41
randal <randal@team-robbins.com>
2024-03-31 13:21:41 -0500

    Test1

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ name_of_unit = "hours"
 
 
 def days_to_units(num_of_days):
-    if num_of_days > 0:
-        return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
-    elif num_of_days == 0:
-        return "You entered zero, please enter a positive number"
+    return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
 
 
 def validate_and_execute():
     if user_input.isdigit():
         user_input_number = int(user_input)
-        calculated_value = days_to_units(user_input_number)
-        print(calculated_value)
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("You entered zero, please enter a positive number")
     else:
         print("Your input is not a valid number. Dont ruin my program")
 
