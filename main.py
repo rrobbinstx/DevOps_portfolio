dbb998f2900c79a7ad2d562dc233b9cc64761f55
randal <randal@team-robbins.com>
2024-03-31 12:53:28 -0500

    dsddsseee

---
+++ main.py
@@ -0,0 +1,18 @@
+calculation_to_units = 24
+name_of_unit = "hours"
+
+
+def days_to_units(num_of_days):
+    if num_of_days > 0:
+        return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
+    elif num_of_days == 0:
+        return "You entered zero, please enter a positive number"
+    else:
+        return 'You entered a negative value, so no conversion for you'
+
+
+user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
+user_input_number = int(user_input)
+
+calculated_value = days_to_units(user_input_number)
+print(calculated_value)
