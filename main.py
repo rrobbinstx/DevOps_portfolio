eb71b2d20c4a71a3f233d6bb5f3b2950d88fe96b
randal <randal@team-robbins.com>
2024-04-01 07:05:16 -0500

    Test1

--- main.py
+++ main.py
@@ -9,17 +9,20 @@ def days_to_units(num_of_days):
 def validate_and_execute():
     try:
 
-        user_input_number = int(user_input)
+        user_input_number = int(num_of_days_element)
         if user_input_number > 0:
             calculated_value = days_to_units(user_input_number)
             print(calculated_value)
         elif user_input_number == 0:
-            print("You entered zero, please enter a positive number")
+            print("You entered 0, please enter a positive number")
         else:
             print("you have entered a negative number, no conversion for you")
     except ValueError:
         print("Your input is not a valid number. Dont ruin my program")
 
 
-user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
-validate_and_execute()
+user_input = ""
+while user_input != "exit":
+    user_input = input("Hey user, enter number of days as a comma seperated list and I will convert it to hours!\n")
+    for num_of_days_element in user_input.split(", "):
+        validate_and_execute()
